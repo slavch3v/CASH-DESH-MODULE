4f2bb4c40b84baa30202ce955c75ad1314d26438
Biser Slavchev <biserslavchev90@gmail.com>
2024-04-05 13:24:13 +0300

    Setup the project

---
+++ cash-operations-module/src/main/java/com/fibank/cashoperationsmodule/CashOperationsModuleApplication.java
@@ -0,0 +1,13 @@
+package com.fibank.cashoperationsmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashOperationsModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashOperationsModuleApplication.class, args);
+	}
+
+}
---
+++ cash-operations-module/src/test/java/com/fibank/cashoperationsmodule/CashOperationsModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fibank.cashoperationsmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashOperationsModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
